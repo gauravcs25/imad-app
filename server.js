eafd5c84ad93b94f8d6cedc1e6e39a07c80b0d57
gauravcs25 <30833585+gauravcs25@users.noreply.github.com>
2017-08-10 14:41:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,12 +84,6 @@ app.get('/:articleName',function (req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two',function (req,res){
-    res.send('Article two requested and will be served here.');
-});
-app.get('/article-three',function (req,res){
-    res.send('Article three requested and will be served here.');
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
